3442e82369571b219f53be8c6490f616058cf2a9
tawanchaiii <tawanchai_champ@hotmail.com>
2023-11-26 19:21:58 +0700

    refactor: add upload path into server

--- src/api/upload.api.ts
+++ src/api/upload.api.ts
@@ -1,14 +1,12 @@
-// import cloudinary from '@/libs/cloudinary';
-import { Env, Picture } from "@/api/types";
-import { Buffer } from "buffer";
-import { detectType,dataURItoUint8Array } from "@/api/utils";
+import { Picture } from '@/api/types';
+import { detectType, dataURItoUint8Array } from '@/api/utils';
+import { Context, Next } from 'hono';
 
-export async function validateFace(request: Request, env: Env): Promise<Response> {
-  const { headers } = request;
-  const contentType = headers.get('content-type');
-  const API_KEY = headers.get('X-Api-Key');
+export async function validateFace(c: Context, next: Next): Promise<Response> {
+  const contentType = c.req.header('content-type');
+  const API_KEY = c.req.header('X-Api-Key');
 
-  if(API_KEY !== env.SHARED_API_KEY){
+  if (API_KEY !== c.env.SHARED_API_KEY) {
     return new Response('Unauthorized', {
       status: 401,
     });
@@ -20,18 +18,17 @@ export async function validateFace(request: Request, env: Env): Promise<Response
     });
   }
 
-  const body: Picture = await request.json();
+  const body: Picture = await c.req.json()
 
   if (!body || !body.pic || !body.face_id) {
     return new Response('Missing required fields.', { status: 400 });
   }
-  const base64 = body.pic
-  const type = detectType(base64)
+  const base64 = body.pic;
+  const type = detectType(base64);
 
-  if (!type)
-    return new Response('Picture not found type', { status: 400 });
+  if (!type) return new Response('Picture not found type', { status: 400 });
 
-  await env.MY_BUCKET.put('image', dataURItoUint8Array(base64), { httpMetadata: { contentType: type.mimeType } });
+  await c.env.MY_BUCKET.put('image', dataURItoUint8Array(base64), { httpMetadata: { contentType: type.mimeType } });
 
   const response = {
     status: Math.random() < 0.5,
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono';
 import { Bindings } from '@/bindings/index';
+import { validateFace } from '@/api/upload.api';
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('/', async (c, next) => c.text('Hello Hono!'));
+
+app.post('/upload', validateFace);
 
 export default app;
